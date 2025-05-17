29a0994ec8858b2d927f4af2aa48a1b1c771e124
Eloquent <ujford@gmail.com>
2025-05-18 00:52:04 +0800

    initialize db

--- main.go
+++ main.go
@@ -11,59 +11,60 @@ var (
 	target_db_file string
 )
 
-// Initialize db
-func InitDB() sql.Result {
+func InitDB() {
 	target_db_file = "./spend.db"
 	var result sql.Result
 	var err error
 
-	DB, err = sql.Open("sql3", target_db_file)
-	assert_error("Error opening sql.Open: %q", err)
+	DB, err = sql.Open("sqlite3", target_db_file)
+	if err != nil {
+		log.Fatalf("Error opening target DB: %q", err)
+	}
 
 	init_table := `
 	CREATE TABLE IF NOT EXISTS daily(
-		id INTEGER PRIMARY KEY AUTOINCREMENT, 
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		item TEXT NOT NULL,
 		amount REAL NOT NULL,
 		date TEXT NOT NULL,
-		tag_id INTEGER NOT NULL,
+		tag_id INTEGER,
+		FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+	);
+	CREATE TABLE IF NOT EXISTS monthly(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		item TEXT NOT NULL,
+		amount REAL NOT NULL,
+		date TEXT NOT NULL,
+		tag_id INTEGER,
 		FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
 	);
 	CREATE TABLE IF NOT EXISTS tags(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL
 	);
+	CREATE TABLE IF NOT EXISTS spend_ahead(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		amount REAL NOT NULL
+	);
 	CREATE TABLE IF NOT EXISTS forecast(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		daily_mean REAL NOT NULL,
 		daily_total REAL NOT NULL,
-		monthly_total REAL NOT NULL,
 		daily_forecast REAL NOT NULL,
-		daily_overshoot REAL NOT NULL,
-		spend_total REAL NOT NULL,
-		income_total REAL NOT NULL
+		monthly_total REAL NOT NULL,
+		ahead_total REAL NOT NULL,
+		income_total REAL NOT NULL,
+		overshoot_total REAL NOT NULL
 	);
-	CREATE TABLE IF NOT EXISTS monthly(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		item TEXT NOT NULL,
-		amount REAL NOT NULL,
-		date TEXT NOT NULL,
-		tag_id INTEGER NOT NULL,
-		FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
-	);`
-
-	result, err = DB.Exec(init_table)
-	assert_error("Error executing DB.Exec: %q", err)
-
-	return result
+	`
 }
 
 func Create(input [5]string) [5]string {
 	return input
 }
 
-// Assert Error
-func assert_error(message string, err error) {
+// log error
+func asser_error(message string, err error) {
 	if err != nil {
 		log.Fatalf(message, err)
 	}
