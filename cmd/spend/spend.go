27b819a29a8746bb4e1ad8b45fc7a852e1be51a5
Eloquent <72538969+UJFord@users.noreply.github.com>
2025-06-24 21:26:11 +0800

    spend as a module

---
+++ cmd/spend/spend.go
@@ -0,0 +1,616 @@
+package spend
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"strconv"
+	"strings"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Represent DB connection
+var (
+	DB             *sql.DB
+	target_db_file string
+)
+
+type Daily struct {
+	id      int64
+	item    string
+	amount  float64
+	date    time.Time
+	tag     Tag
+	isDaily bool
+}
+
+type DailyActions interface {
+	Create() (Daily, error)
+	Read() (Daily, error)
+	Edit(int, any) (Daily, error)
+	Remove() (Daily, error)
+}
+
+type Ahead struct {
+	id     int64
+	amount float64
+	date   time.Time
+}
+
+type AheadActions interface {
+	Create() (Ahead, error)
+	Read() (Ahead, error)
+	Remove() (Ahead, error)
+}
+
+type Tag struct {
+	id   int64
+	name string
+}
+
+type TagActions interface {
+	SetID() (Tag, error)
+	SetValue() (Tag, error)
+
+	Edit(string) (Tag, error)
+}
+
+type Income struct {
+	id     int64
+	amount float64
+	date   time.Time
+}
+
+type IncomeActions interface {
+	Create() (Income, error)
+	Read() (Income, error)
+	Edit(int, any) (Income, error)
+	Remove() (Income, error)
+}
+
+type Forecast struct {
+	daily_total     float64
+	ahead_total     float64
+	income_total    float64
+	daily_forecast  float64
+	daily_mean      float64
+	overshoot_total float64
+}
+
+type ForecastActions interface {
+	Update() (Forecast, error)
+}
+
+// Initalize db
+func InitDB() error {
+	target_db_file = "./spend.db"
+	var err error
+
+	DB, err = sql.Open("sqlite3", target_db_file)
+	if err != nil {
+		return fmt.Errorf("error initializing db: '%w'", err)
+	}
+
+	init_schema := `
+	CREATE TABLE IF NOT EXISTS spend(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		item TEXT NOT NULL,
+		amount REAL NOT NULL,
+		date TEXT DEFAULT '1970-01-01 00:00:00+00:00',
+		tag_id INTEGER,
+		is_daily INTEGER DEFAULT '1',
+		FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+	);
+	CREATE TABLE IF NOT EXISTS tags(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name TEXT DEFAULT 'unnamed'
+	);
+	CREATE TABLE IF NOT EXISTS ahead(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		amount REAL NOT NULL,
+		date TEXT DEFAULT '1970-01-01 00:00:00+00:00'
+	);
+	CREATE TABLE IF NOT EXISTS income(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		amount REAL NOT NULL,
+		date TEXT DEFAULT '1970-01-01 00:00:00+00:00'
+	);
+	`
+	_, err = DB.Exec(init_schema)
+	if err != nil {
+		return fmt.Errorf("error executing init db: '%w'", err)
+	}
+
+	return nil
+}
+
+// Create
+func (s Daily) Create() (Daily, error) {
+
+	var err error
+	s.tag, err = s.tag.SetID()
+	if err != nil {
+		return Daily{}, fmt.Errorf("create error getting tag id: '%w'", err)
+	}
+
+	insert_stmt, err := DB.Prepare(fmt.Sprintf(`
+		INSERT INTO spend(item, amount, date, tag_id, is_daily)
+		VALUES (?, ?, ?, ?, ?)
+	`))
+	if err != nil {
+		return Daily{}, fmt.Errorf("create error preparing insert statement: '%w'", err)
+	}
+	defer insert_stmt.Close()
+
+	exec_insert_stmt, err := insert_stmt.Exec(s.item, s.amount, s.date, s.tag.id, s.isDaily)
+	if err != nil {
+		return Daily{}, fmt.Errorf("create error executing insert statement: '%w'", err)
+	}
+
+	s.id, err = exec_insert_stmt.LastInsertId()
+	if err != nil {
+		return Daily{}, fmt.Errorf("create error fetching last insert id: '%w'", err)
+	}
+
+	return s, nil
+}
+
+func (a Ahead) Create() (Ahead, error) {
+
+	insert_stmt, err := DB.Prepare("INSERT INTO ahead(amount, date) VALUES(?, ?)")
+
+	if err != nil {
+		return Ahead{}, fmt.Errorf("create ahead error preparing insert statement: '%w'", err)
+	}
+	defer insert_stmt.Close()
+
+	exec_insert_stmt, err := insert_stmt.Exec(a.amount, a.date)
+	if err != nil {
+		return Ahead{}, fmt.Errorf("create ahead error executing insert statement: '%w'", err)
+	}
+
+	id_of_inserted, err := exec_insert_stmt.LastInsertId()
+	if err != nil {
+		return Ahead{}, fmt.Errorf("create ahead error fetching last insert id: '%w'", err)
+	}
+
+	a.id = id_of_inserted
+
+	return a, nil
+}
+
+// Get info by id
+func (s Daily) Read() (Daily, error) {
+
+	get := DB.QueryRow("SELECT item, amount, date, tag_id, is_daily FROM spend WHERE id=?", s.id)
+
+	var unparsed_date string
+	err := get.Scan(&s.item, &s.amount, &unparsed_date, &s.tag.id, &s.isDaily)
+	if err != nil {
+		return Daily{}, fmt.Errorf("read error scanning read query: '%w'", err)
+	}
+
+	layout := "2006-01-02 15:04:05-07:00"
+	s.date, err = time.Parse(layout, unparsed_date)
+	if err != nil {
+		return Daily{}, fmt.Errorf("read error parsing date from db: '%w'", err)
+	}
+
+	s.tag, err = s.tag.SetValue()
+	if err != nil {
+		return Daily{}, err
+	}
+
+	return s, nil
+}
+
+func (a Ahead) Read() (Ahead, error) {
+	get := DB.QueryRow("SELECT amount, date FROM ahead WHERE id=?", a.id)
+
+	var unparsed_date string
+	err := get.Scan(&a.amount, &unparsed_date)
+	if err != nil {
+		return Ahead{}, fmt.Errorf("read ahead error scanning query stetement: '%w'", err)
+	}
+
+	layout := "2006-01-02 15:04:05-07:00"
+	a.date, err = time.Parse(layout, unparsed_date)
+	if err != nil {
+		return Ahead{}, fmt.Errorf("read error parsing date from db: '%w'", err)
+	}
+
+	return a, nil
+}
+
+// Edit a spend
+func (s Daily) Edit(target_field int, replace_value any) (Daily, error) {
+
+	var err error
+
+	if target_field < 0 || target_field > 4 {
+		return Daily{}, errors.New("EDIT error choosing target info: Out of Bounds")
+	}
+
+	var target string
+	switch target_field {
+	case 0:
+		target = "item"
+	case 1:
+		target = "amount"
+	case 2:
+		target = "date"
+	case 3:
+		target = "tag_id"
+
+		if str_value, ok := replace_value.(string); ok {
+
+			s.tag.name = str_value
+
+			s.tag, err = s.tag.SetID()
+			if err != nil {
+				return Daily{}, err
+			}
+
+			replace_value = strconv.FormatInt(s.tag.id, 10)
+
+		}
+	case 4:
+		target = "is_daily"
+	}
+
+	edit_stmt, err := DB.Prepare(fmt.Sprintf("UPDATE spend SET %s = ? WHERE id = ?", target))
+	if err != nil {
+		return Daily{}, fmt.Errorf("edit error preparing edit statement: '%w'", err)
+	}
+	defer edit_stmt.Close()
+
+	_, err = edit_stmt.Exec(replace_value, s.id)
+	if err != nil {
+		return Daily{}, fmt.Errorf("edit error executing edit statement: '%w'", err)
+	}
+
+	s, err = s.Read()
+	if err != nil {
+		return Daily{}, err
+	}
+
+	return s, nil
+}
+
+// Remove spend
+func (s Daily) Remove() (Daily, error) {
+
+	s, err := s.Read()
+	if err != nil {
+		return Daily{}, err
+	}
+
+	delete_stmt, err := DB.Prepare("DELETE FROM spend WHERE id=?")
+	if err != nil {
+		return Daily{}, fmt.Errorf("remove error preparing delete statement: '%w'", err)
+	}
+
+	_, err = delete_stmt.Exec(s.id)
+	if err != nil {
+		return Daily{}, fmt.Errorf("remove error executing delete statement: '%w'", err)
+	}
+
+	return s, nil
+}
+
+func (a Ahead) Remove() (Ahead, error) {
+
+	a, err := a.Read()
+	if err != nil {
+		return Ahead{}, err
+	}
+
+	delete_stmt, err := DB.Prepare("DELETE FROM ahead WHERE id=?")
+	if err != nil {
+		return Ahead{}, fmt.Errorf("remove error preparing delete statement: '%w'", err)
+	}
+
+	_, err = delete_stmt.Exec(a.id)
+	if err != nil {
+		return Ahead{}, fmt.Errorf("remove error executing delete statement: '%w'", err)
+	}
+
+	return a, nil
+}
+
+// Forecast
+func (f Forecast) Update() (Forecast, error) {
+	// ahead sum
+	get_total := func(table string) (float64, error) {
+
+		query := fmt.Sprintf(`
+			SELECT SUM(amount) FROM %s
+			WHERE strftime('%%Y-%%m', date) = strftime('%%Y-%%m', 'now')
+		`, table)
+
+		var total sql.NullFloat64
+		err := DB.QueryRow(query).Scan(&total)
+		if err != nil {
+			return 0, err
+		}
+
+		if !total.Valid {
+			return 0, nil
+		}
+
+		return total.Float64, nil
+	}
+
+	get_mean := func() (float64, error) {
+		query := `
+			SELECT SUM(amount) FROM ahead
+			WHERE strftime('%%Y-%%m', date) = strftime('%%Y-%%m', 'now')
+		`
+
+		var total sql.NullFloat64
+		err := DB.QueryRow(query).Scan(&total)
+		if err != nil {
+			return 0, err
+		}
+
+		if total.Valid {
+			return total.Float64, nil
+		}
+		return 0, nil
+	}
+
+	days_left := func() int {
+		today := time.Now()
+		year, month := today.Year(), today.Month()
+
+		first_of_next_month := time.Date(year, month+1, 1, 0, 0, 0, 0, today.Location())
+		days_left := int(first_of_next_month.Sub(today).Hours() / 24)
+
+		return days_left
+	}
+
+	var err error
+
+	f.daily_total, err = get_total("spend")
+	if err != nil {
+		return Forecast{}, fmt.Errorf("forecast error fetching daily total: '%w'", err)
+	}
+
+	f.daily_mean, err = get_mean()
+	if err != nil {
+		return Forecast{}, fmt.Errorf("forecast error fetching daily mean: '%w'", err)
+	}
+
+	f.ahead_total, err = get_total("ahead")
+	if err != nil {
+		return Forecast{}, fmt.Errorf("forecast error fetching ahead mean: '%w'", err)
+	}
+
+	f.income_total, err = get_total("income")
+	if err != nil {
+		return Forecast{}, fmt.Errorf("forecast error fetching income total: '%w'", err)
+	}
+
+	f.daily_forecast = (f.income_total - (f.daily_total + f.ahead_total)) / (float64(days_left()))
+
+	f.overshoot_total = f.income_total - ((f.daily_total + f.ahead_total) + (f.daily_mean * (float64(days_left()))))
+
+	return f, nil
+}
+
+// DATE
+func UnparseDate(time_struct string) (string, error) {
+
+	t, err := time.Parse("2006-01-02 15:04:05-07:00", time_struct)
+	if err != nil {
+		return "", fmt.Errorf("unparse date error parsing date: '%w'", err)
+	}
+
+	date := t.Format("1-2-2006")
+
+	return date, nil
+
+}
+
+func ParseDate(unparsed string) (time.Time, error) {
+	split_unparsed := strings.Split(unparsed, "-")
+
+	month, err := strconv.Atoi(split_unparsed[0])
+	if err != nil {
+		return time.Time{}, errors.New("error converting month to int")
+	}
+
+	day, err := strconv.Atoi(split_unparsed[1])
+	if err != nil {
+		return time.Time{}, errors.New("error converting day to int")
+	}
+
+	year, err := strconv.Atoi(split_unparsed[2])
+	if err != nil {
+		return time.Time{}, errors.New("error converting year to int")
+	}
+
+	t := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+	if !(t.Year() == year && int(t.Month()) == month && t.Day() == day) {
+		return time.Time{}, errors.New("ivalid date format")
+	}
+
+	return t, nil
+}
+
+// TAG
+func (t Tag) Edit(replace string) (Tag, error) {
+
+	var err error
+
+	t_old, err := t.SetID()
+	if err != nil {
+		return Tag{}, err
+	}
+
+	t_new := Tag{name: replace}
+	t_new, err = t_new.SetID()
+	if err != nil {
+		return Tag{}, err
+	}
+
+	replace_stmt, err := DB.Prepare("UPDATE spend SET tag_id = ? WHERE tag_id = ?")
+	if err != nil {
+		return Tag{}, fmt.Errorf("tag edit error preparing replace statement: '%w'", err)
+	}
+	defer replace_stmt.Close()
+
+	_, err = replace_stmt.Exec(t_new.id, t_old.id)
+	if err != nil {
+		return Tag{}, fmt.Errorf("tag edit error executing replace statement: '%w'", err)
+	}
+
+	return t_new, nil
+}
+
+func (t Tag) SetValue() (Tag, error) {
+
+	get_tag_name := DB.QueryRow("SELECT name FROM tags WHERE id=?", t.id)
+
+	err := get_tag_name.Scan(&t.name)
+	if err != nil {
+		return Tag{}, fmt.Errorf("tag value error scanning get tag name statement: '%w'", err)
+	}
+
+	return t, nil
+
+}
+
+func (t Tag) SetID() (Tag, error) {
+
+	err := DB.QueryRow("SELECT id FROM tags WHERE name = ?", t.name).Scan(&t.id)
+
+	if err == sql.ErrNoRows {
+
+		statement, err := DB.Prepare("INSERT INTO tags(name) VALUES (?)")
+		if err != nil {
+			return Tag{}, fmt.Errorf("tag error preparing insert statement: '%w'", err)
+		}
+		defer statement.Close()
+
+		result, err := statement.Exec(t.name)
+		if err != nil {
+			return Tag{}, fmt.Errorf("tag error executing insert statement: '%w'", err)
+		}
+
+		t.id, err = result.LastInsertId()
+		if err != nil {
+			return Tag{}, fmt.Errorf("tag error fetching last insert id: '%w'", err)
+		}
+
+	} else if err != nil {
+
+		return Tag{}, fmt.Errorf("tag error query tag name: '%w'", err)
+
+	}
+
+	return t, nil
+
+}
+
+// INCOME
+func (i Income) Create() (Income, error) {
+
+	create_stmt, err := DB.Prepare(`
+		INSERT INTO income(amount, date)
+		VALUES(?, ?)
+	`)
+	if err != nil {
+		return Income{}, fmt.Errorf("income add error preparing create statement: '%w'", err)
+	}
+	defer create_stmt.Close()
+
+	exec_create_stmt, err := create_stmt.Exec(i.amount, i.date)
+	if err != nil {
+		return Income{}, fmt.Errorf("income add error executing create statement: '%w'", err)
+	}
+
+	i.id, err = exec_create_stmt.LastInsertId()
+	if err != nil {
+		return Income{}, fmt.Errorf("income add error fetching last insert id: '%w'", err)
+	}
+
+	return i, nil
+}
+
+func (i Income) Read() (Income, error) {
+
+	get := DB.QueryRow("SELECT amount, date FROM income WHERE id=?", i.id)
+
+	var unparsed_date string
+	err := get.Scan(&i.amount, &unparsed_date)
+	if err != nil {
+		return Income{}, fmt.Errorf("read ahead error scanning query stetement: '%w'", err)
+	}
+
+	layout := "2006-01-02 15:04:05-07:00"
+	i.date, err = time.Parse(layout, unparsed_date)
+	if err != nil {
+		return Income{}, fmt.Errorf("read error parsing date from db: '%w'", err)
+	}
+
+	return i, nil
+}
+
+func (i Income) Edit(target_field int, value any) (Income, error) {
+
+	var err error
+
+	if target_field < 0 || target_field > 1 {
+		return Income{}, errors.New("EDIT error choosing target info: Out of Bounds")
+	}
+
+	var target string
+	switch target_field {
+	case 0:
+		target = "amount"
+	case 1:
+		target = "date"
+	}
+
+	edit_stmt, err := DB.Prepare(fmt.Sprintf("UPDATE income SET %s=? WHERE id=?", target))
+	if err != nil {
+		return Income{}, fmt.Errorf("edit error preparing edit statement: '%w'", err)
+	}
+	defer edit_stmt.Close()
+
+	_, err = edit_stmt.Exec(value, i.id)
+	if err != nil {
+		return Income{}, fmt.Errorf("edit error executing edit statement: '%w'", err)
+	}
+
+	i, err = i.Read()
+	if err != nil {
+		return Income{}, err
+	}
+
+	return i, nil
+}
+
+func (i Income) Remove() (Income, error) {
+
+	i, err := i.Read()
+	if err != nil {
+		return Income{}, err
+	}
+
+	delete_stmt, err := DB.Prepare("DELETE FROM income WHERE id=?")
+	if err != nil {
+		return Income{}, fmt.Errorf("remove error preparing delete statement: '%w'", err)
+	}
+
+	_, err = delete_stmt.Exec(i.id)
+	if err != nil {
+		return Income{}, fmt.Errorf("remove error executing delete statement: '%w'", err)
+	}
+
+	return i, nil
+}
---
+++ cmd/spend/spend_test.go
@@ -0,0 +1,447 @@
+package spend
+
+import (
+	"testing"
+	"time"
+)
+
+var (
+	daily_inserted_id   int64
+	monthly_inserted_id int64
+	ahead_inserted_id   int64
+	income_inserted_id  int64
+)
+var date = time.Date(2011, 11, 30, 0, 0, 0, 0, time.Local)
+var new_date = time.Date(2012, 12, 30, 0, 0, 0, 0, time.Local)
+
+// Daily
+func TestDailyCreate(t *testing.T) {
+
+	InitDB()
+
+	create_tests := []struct {
+		name     string
+		spend    Daily
+		expected string
+	}{
+		{name: "daily",
+			spend: Daily{0, "daily item", 60.0, date, Tag{name: "testing"}, true},
+		},
+		{name: "monthly",
+			spend: Daily{0, "monthly item", 123.0, date, Tag{name: "testing"}, false},
+		},
+	}
+
+	for _, tt := range create_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.spend.Create()
+
+			tt.spend.tag, err = tt.spend.tag.SetID()
+			if err != nil {
+				t.Error(err)
+			}
+
+			if got.isDaily {
+				daily_inserted_id = got.id
+				tt.spend.id = daily_inserted_id
+			} else {
+
+				monthly_inserted_id = got.id
+				tt.spend.id = monthly_inserted_id
+			}
+
+			if err != nil {
+				t.Error(err)
+			}
+
+			want := tt.spend
+
+			if got != want {
+				t.Errorf("got %#v want %#v", got, want)
+			}
+		})
+	}
+}
+
+func TestDailyRead(t *testing.T) {
+
+	read_tests := []struct {
+		name  string
+		spend Daily
+	}{
+		{name: "daily",
+			spend: Daily{daily_inserted_id, "daily item", 60.0, date, Tag{name: "testing"}, true},
+		},
+		{name: "monthly",
+			spend: Daily{monthly_inserted_id, "monthly item", 123.0, date, Tag{name: "testing"}, false},
+		},
+	}
+
+	for _, tt := range read_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.spend.Read()
+			if err != nil {
+				t.Error(err)
+			}
+
+			tt.spend.tag, err = tt.spend.tag.SetID()
+			if err != nil {
+				t.Error(err)
+			}
+
+			want := tt.spend
+
+			if got != tt.spend {
+				t.Errorf("got %#v want %#v", got, want)
+			}
+		})
+	}
+}
+
+func TestDailyEdit(t *testing.T) {
+
+	edit_tests := []struct {
+		name   string
+		before Daily
+		value  any
+		field  int
+		after  Daily
+	}{
+		{name: "daily",
+			before: Daily{id: daily_inserted_id},
+			field:  0,
+			value:  "daily replacement",
+			after:  Daily{daily_inserted_id, "daily replacement", 60.0, date, Tag{name: "testing"}, true},
+		},
+		{name: "tag replace",
+			before: Daily{id: monthly_inserted_id},
+			field:  3,
+			value:  "tag replacement",
+			after:  Daily{monthly_inserted_id, "monthly item", 123.0, date, Tag{name: "tag replacement"}, false},
+		},
+		{name: "monthly",
+			before: Daily{id: monthly_inserted_id},
+			field:  1,
+			value:  456.7,
+			after:  Daily{monthly_inserted_id, "monthly item", 456.7, date, Tag{name: "tag replacement"}, false},
+		},
+	}
+
+	for _, tt := range edit_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			var err error
+
+			tt.before, err = tt.before.Read()
+			if err != nil {
+				t.Error(err)
+			}
+
+			got, err := tt.before.Edit(tt.field, tt.value)
+			if err != nil {
+				t.Error(err)
+			}
+
+			tt.after.tag, err = tt.after.tag.SetID()
+			if err != nil {
+				t.Error(err)
+			}
+
+			want := tt.after
+
+			if got != want {
+				t.Errorf("got %#v want %#v", got, want)
+			}
+		})
+	}
+}
+
+func TestDailyRemove(t *testing.T) {
+
+	remove_test := []struct {
+		name  string
+		spend Daily
+	}{
+		{name: "daily",
+			spend: Daily{daily_inserted_id, "daily replacement", 60.0, date, Tag{name: "testing"}, true},
+		},
+		{name: "monthly",
+			spend: Daily{monthly_inserted_id, "monthly item", 456.7, date, Tag{name: "tag replacement"}, false},
+		},
+	}
+
+	for _, tt := range remove_test {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.spend.Remove()
+			if err != nil {
+				t.Error(err)
+			}
+
+			tt.spend.tag.id = got.tag.id
+
+			want := tt.spend
+
+			if got != want {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
+
+// Spending Ahead
+func TestAheadCreate(t *testing.T) {
+
+	create_tests := []struct {
+		name  string
+		spend Ahead
+	}{
+		{name: "create",
+			spend: Ahead{0, 999.0, date},
+		},
+	}
+
+	for _, tt := range create_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.spend.Create()
+			if err != nil {
+				t.Error(err)
+			}
+
+			ahead_inserted_id = got.id
+			tt.spend.id = ahead_inserted_id
+
+			want := tt.spend
+
+			if got != want {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
+
+func TestAheadRead(t *testing.T) {
+
+	read_tests := []struct {
+		name  string
+		spend Ahead
+	}{
+		{name: "read",
+			spend: Ahead{ahead_inserted_id, 999.0, date},
+		},
+	}
+
+	for _, tt := range read_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.spend.Read()
+			if err != nil {
+				t.Error(err)
+			}
+
+			want := tt.spend
+
+			if got != tt.spend {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
+
+func TestAheadRemove(t *testing.T) {
+
+	remove_tests := []struct {
+		name  string
+		spend Ahead
+	}{
+		{name: "remove",
+			spend: Ahead{ahead_inserted_id, 999.0, date},
+		},
+	}
+
+	for _, tt := range remove_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.spend.Read()
+			if err != nil {
+				t.Error(err)
+			}
+
+			want := tt.spend
+
+			if got != want {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
+
+// Tags
+func TestTagEdit(t *testing.T) {
+
+	edit_tests := []struct {
+		name    string
+		tag     Tag
+		replace string
+		new_tag Tag
+	}{
+		{name: "editing",
+			tag:     Tag{0, "testing"},
+			replace: "testing tag edit",
+			new_tag: Tag{0, "testing tag edit"},
+		},
+		{name: "unnaming",
+			tag:     Tag{0, "testing tag edit"},
+			replace: "",
+			new_tag: Tag{0, ""},
+		},
+	}
+
+	for _, tt := range edit_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.tag.Edit(tt.replace)
+			if err != nil {
+				t.Error(err)
+			}
+
+			tt.new_tag.id = got.id
+
+			want := tt.new_tag
+
+			if got != want {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
+
+// Income
+func TestIncomeCreate(t *testing.T) {
+
+	create_tests := []struct {
+		name   string
+		income Income
+	}{
+		{name: "10K",
+			income: Income{0, 10000.0, date},
+		},
+	}
+
+	for _, tt := range create_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.income.Create()
+			if err != nil {
+				t.Error(err)
+			}
+
+			income_inserted_id = got.id
+			tt.income.id = income_inserted_id
+
+			want := tt.income
+
+			if got != want {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
+
+func TestIncomeRead(t *testing.T) {
+
+	read_tests := []struct {
+		name   string
+		income Income
+	}{
+		{name: "10K",
+			income: Income{income_inserted_id, 10000.0, date},
+		},
+	}
+
+	for _, tt := range read_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.income.Read()
+			if err != nil {
+				t.Error(err)
+			}
+
+			want := tt.income
+
+			if got != want {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
+
+func TestIncomeEdit(t *testing.T) {
+
+	edit_tests := []struct {
+		name         string
+		before       Income
+		target_field int
+		value        any
+		after        Income
+	}{
+		{name: "amount",
+			before:       Income{id: income_inserted_id},
+			target_field: 0,
+			value:        6969.7,
+			after:        Income{income_inserted_id, 6969.7, date},
+		},
+		{name: "date",
+			before:       Income{id: income_inserted_id},
+			target_field: 1,
+			value:        new_date,
+			after:        Income{income_inserted_id, 6969.7, new_date},
+		},
+	}
+
+	for _, tt := range edit_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.before.Edit(tt.target_field, tt.value)
+			if err != nil {
+				t.Error(err)
+			}
+
+			want := tt.after
+
+			if got != want {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
+
+func TestIncomeRemove(t *testing.T) {
+
+	remove_tests := []struct {
+		name   string
+		income Income
+	}{
+		{name: "remove created",
+			income: Income{income_inserted_id, 6969.7, new_date}},
+	}
+
+	for _, tt := range remove_tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := tt.income.Remove()
+			if err != nil {
+				t.Error(err)
+			}
+
+			want := tt.income
+
+			if got != want {
+				t.Errorf("got '%#v' want '%#v'", got, want)
+			}
+		})
+	}
+}
